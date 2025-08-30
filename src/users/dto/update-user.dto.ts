dbf78ff555c53fc1d5493ef3a110a1d0b57cc483
Jero <socger@gmail.com>
2025-08-30 10:52:32 +0200

    refactor: Improve import formatting in UpdateUserDto for better readability

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,5 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsOptional, IsBoolean, IsString, IsEmail, MinLength, MaxLength, IsArray, IsInt } from 'class-validator';
+import {
+  IsOptional,
+  IsBoolean,
+  IsString,
+  IsEmail,
+  MinLength,
+  MaxLength,
+  IsArray,
+  IsInt,
+} from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
