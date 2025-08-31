6d26256af18dad35092df6e1d3e8e38bae84248a
Jero <socger@gmail.com>
2025-08-31 14:54:57 +0200

    feat: Add RefreshToken entity to database configuration

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,6 +1,7 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
 import { Role } from '../entities/role.entity';
+import { RefreshToken } from '../entities/refresh-token.entity';
 
 export const databaseConfig = (): TypeOrmModuleOptions => ({
   type: 'mysql',
@@ -9,7 +10,7 @@ export const databaseConfig = (): TypeOrmModuleOptions => ({
   username: process.env.DB_USERNAME || 'socger',
   password: process.env.DB_PASSWORD || 'dcb4f2e8106a0ef44c3f530d3ae3f9fd',
   database: process.env.DB_DATABASE || 'socgerfleet',
-  entities: [User, Role],
+  entities: [User, Role, RefreshToken],
   synchronize: process.env.NODE_ENV === 'development',
   logging: process.env.NODE_ENV === 'development',
 });
